111fb9fb3b1b0c6e024d47f4750437e38517ec9b
蒋华军 <jianghuajun@jianghuajundeMacBook-Pro.local>
2016-12-23 17:21:00 +0800

    create
    创建

---
+++ JHNetRequest/JHNetRequest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  JHNetRequest
+//
+//  Created by 蒋华军 on 16/12/23.
+//  Copyright © 2016年 499080255@qq.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ JHNetRequest/JHNetRequest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  JHNetRequest
+//
+//  Created by 蒋华军 on 16/12/23.
+//  Copyright © 2016年 499080255@qq.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
